4c909e2296e136288e25c6cc812bad5de0c6719c
George Buckley <georgerdbuckley@gmail.com>
2022-01-09 11:26:10 +0000

    Add app_token to configuration

--- lib/callcounter/configuration.rb
+++ lib/callcounter/configuration.rb
@@ -3,11 +3,12 @@
 module Callcounter
   # configuration class for Callcounter middleware
   class Configuration
-    attr_accessor :debug, :project_token, :track, :async
+    attr_accessor :debug, :project_token, :track, :async, :app_token
 
     def initialize
       @debug = false
       @project_token = nil
+      @app_token = nil
       @track = lambda { |request|
         return request.hostname.start_with?('api') || request.path.start_with?('/api')
       }
